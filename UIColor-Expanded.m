5d57533748f51e1ba0870611c7b57152b4878fa9
Kevin Ballard <kevin@sb.org>
2009-02-06 16:25:50 -0800

    Reimplement +colorWithHexString using NSScanner
    Instead of parsing the hex string into components, just convert
    the whole thing into one int and extract the bytes from there.
    It's much shorter and more efficient.

--- UIColor-Expanded.m
+++ UIColor-Expanded.m
@@ -220,38 +220,14 @@
 
 + (UIColor *) colorWithHexString: (NSString *) stringToConvert
 {
-	NSString *cString = [[stringToConvert stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
-	
-	// String should be 6 or 8 characters
-	if ([cString length] < 6) return DEFAULT_VOID_COLOR;
-	
-	// strip 0X if it appears
-	if ([cString hasPrefix:@"0X"]) cString = [cString substringFromIndex:2];
-	
-	if ([cString length] != 6) return DEFAULT_VOID_COLOR;
-
-	// Separate into r, g, b substrings
-	NSRange range;
-	range.location = 0;
-	range.length = 2;
-	NSString *rString = [cString substringWithRange:range];
-	
-	range.location = 2;
-	NSString *gString = [cString substringWithRange:range];
-	
-	range.location = 4;
-	NSString *bString = [cString substringWithRange:range];
-	
-	// Scan values
-	unsigned int r, g, b;
-	[[NSScanner scannerWithString:rString] scanHexInt:&r];
-	[[NSScanner scannerWithString:gString] scanHexInt:&g];
-	[[NSScanner scannerWithString:bString] scanHexInt:&b];
-	
-	return [UIColor colorWithRed:((float) r / 255.0f)
-						   green:((float) g / 255.0f)
-							blue:((float) b / 255.0f)
-						   alpha:1.0f];
+	NSScanner *scanner = [NSScanner scannerWithString:stringToConvert];
+	unsigned hexNum;
+	if (![scanner scanHexInt:&hexNum]) return nil;
+	CGFloat r, g, b;
+	r = ((hexNum & 0xFF0000) >> 16) / 255.0f;
+	g = ((hexNum & 0x00FF00) >> 8) / 255.0f;
+	b = (hexNum & 0x0000FF) / 255.0f;
+	return [UIColor colorWithRed:r green:g blue:b alpha:1.0f];
 }
 @end
 
